72a4684ca6407579d8f0cb354eedaacefac74cf6
Sinval Silva Junior <54493316+kjunda01@users.noreply.github.com>
2024-04-25 16:30:15 -0300

    Update urls.py

--- djangoapp/urls.py
+++ djangoapp/urls.py
@@ -10,20 +10,7 @@ urlpatterns = [
     path ('read/<str:pk>', views.getUser),
     path ('update/<str:pk>', views.updateUser),
     path ('delete/<str:pk>', views.deleteUser),
-    path('', TemplateView.as_view(template_name='index.html')),
+    path ('', TemplateView.as_view(template_name='index.html')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
-'''
-from django.urls import path
-from .views import atualizar_excluir_inscricao, obter_inscricao, listar_criar_inscricoes
-
-urlpatterns = [
-    path('inscricoes/', listar_criar_inscricoes, name='listar_criar_inscricoes'),
-    path('inscricoes/<int:inscricao_id>/', obter_inscricao, name='obter_inscricao'),
-    path('inscricoes/<int:inscricao_id>/', atualizar_excluir_inscricao, name='atualizar_excluir_inscricao'),
-]
-
-'''
