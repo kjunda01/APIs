b708086825f5deca38e7c92fcf20f30845f13418
Sinval Silva Junior <54493316+kjunda01@users.noreply.github.com>
2024-04-25 15:06:28 -0300

    Update django.sh

--- django.sh
+++ django.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 echo "Creating Migrations..."
-python3 manage.py makemigrations djangoapp
+python3 manage.py makemigrations
 echo ====================================
 
 echo "Starting Migrations..."
 python3 manage.py migrate
 echo ====================================
+
+echo "Starting server"
+echo ====================================
+python3 manage.py runserver 0.0.0.0:8000
