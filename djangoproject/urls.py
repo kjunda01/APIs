47d06b2c6787662cff62301aae7a3454dbfd8d2d
Sinval Silva Junior <54493316+kjunda01@users.noreply.github.com>
2024-04-25 16:30:48 -0300

    Update urls.py

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -3,18 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('djangoapp.urls')),
+    path('api/users/', include('djangoapp.urls')),
 ]
-
-'''
-from django.contrib import admin
-from django.urls import path, include
-from djangoapp.urls import urlpatterns as djangoapp_urls
-
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    # Inclua as URLs do aplicativo 'djangoapp_urls'
-    path('api/', include(djangoapp_urls)),
-]
-'''
